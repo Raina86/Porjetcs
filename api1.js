912c7d791bfd839aaf1231bb128b31e081c2299a
Raina86 <you@example.com>
2020-09-30 11:37:55 -0400

    deploying-api1

---
+++ api1.js
@@ -0,0 +1,29 @@
+let cardImg = document.querySelector(".card-img");
+let name = document.querySelector(".name");
+let ability = document.querySelector(".name");
+let generateBtn = document.querySelector(".submit-btn");
+
+generateBtn.addEventListener("click", e => {
+    e.preventDefault();
+
+    let id = document.querySelector(".search-input").value;
+
+    async function getPokemon() {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const data = await res.json();
+        console.log(data.name)
+        let img = data.sprites.front_default;
+        let newName = data.name;
+        let newAbility = data.abilities[0].ability.name;
+
+        // newName = newName.charAt(0).toUpperCase() + newName.slice(1);
+        name.value = newName
+
+        newAbility = newAbility.charAt(0).toUpperCase() + newAbility.slice(1);
+        ability.innerText = newAbility;
+
+        cardImg.innerHTML = `<img src-${img}`;
+    }
+
+    getPokemon();
+});
